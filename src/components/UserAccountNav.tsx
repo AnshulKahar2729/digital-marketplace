45adcc6cfb213967cec6973f6d5d38ebd2bf6e66
Anshul Kahar <22511126.dypit@dypvp.edu.in>
2024-07-02 01:14:40 +0530

    signup page build navbar build

---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+interface UserAccountNavProps {
+  user: boolean;
+}
+
+const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
+  return <div></div>;
+};
+
+export default UserAccountNav;
